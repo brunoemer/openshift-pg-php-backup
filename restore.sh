f07471c497df13ef5cfaf2e226d2e39f632a3409
Bruno <emerbruno@gmail.com>
2019-04-20 15:51:07 -0300

    Restore fixes

--- restore.sh
+++ restore.sh
@@ -7,11 +7,12 @@ project=default
 pg_db_name=moodle
 
 oc rsync moodle/db/ $pod_pg:/opt/app-root/src -n $project
-#dump postgres
-#echo ": psql -f $file_pg moodle"
+#oc rsh -n $project $pod_pg /bin/sh -i -c "psql -f $file_pg $pg_db_name"
+
 #oc exec $pod_pg -n $project -it -- bash
-oc rsh -n $project $pod_pg /bin/sh -i -c "psql -f $file_pg $pg_db_name"
+#psql moodle -c "select 'drop table if exists \"' || tablename || '\" cascade;' from pg_tables where schemaname='public';" > /tmp/drop.tmp.sql
+#psql -f /tmp/drop.tmp.sql moodle
+#psql -f backup.sql moodle
 
-oc rsync moodle/ $pod:/opt/app-root/ -n $project
-
+oc rsync moodle/etc/ $pod:/opt/app-root/etc/ -n $project
 
