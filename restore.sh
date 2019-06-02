eb8ee72948de9cd620fb1571560c0a50cc19b1de
Bruno <emerbruno@gmail.com>
2019-06-02 15:55:07 -0300

    restore backup psql

--- restore.sh
+++ restore.sh
@@ -7,9 +7,11 @@ project=default
 pg_db_name=moodle
 
 oc rsync moodle/db/ $pod_pg:/opt/app-root/src -n $project
+
 #oc rsh -n $project $pod_pg /bin/sh -i -c "psql -f $file_pg $pg_db_name"
 
 #oc exec $pod_pg -n $project -it -- bash
+#psql -c "create user moodle with encrypted password 'mypass';"
 #psql moodle -c "select 'drop table if exists \"' || tablename || '\" cascade;' from pg_tables where schemaname='public';" > /tmp/drop.tmp.sql
 #psql -f /tmp/drop.tmp.sql moodle
 #psql -f backup.sql moodle
